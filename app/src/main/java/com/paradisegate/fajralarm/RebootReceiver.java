2e5e26f1003e159b065200ac69f9f06b47789b8f
KarimAter <Katerelsherif@gmail.com>
2019-05-01 20:10:52 +0200

    Version 0.8

--- app/src/main/java/com/paradisegate/fajralarm/RebootReceiver.java
+++ app/src/main/java/com/paradisegate/fajralarm/RebootReceiver.java
@@ -13,23 +13,26 @@ import static com.paradisegate.fajralarm.Constants.dateFormat;
 public class RebootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        String firstAlarmTime = Utils.getFirstCallTime(context);
-        Log.d("CallingService", "onReceive: " + firstAlarmTime);
-        if (!firstAlarmTime.isEmpty() && Utils.isAlarmsScheduled(context))
-            //Notification Alarm
-            Utils.setPersonalAlarm(context,
-                    Utils.convertCalendarToString(Utils.convertStringToCalendar(firstAlarmTime, dateFormat)
-                            , dateFormat));
-        DatabaseHelper databaseHelper = new DatabaseHelper(context);
-        ArrayList<Contact> selectedContacts = databaseHelper.loadContacts();
-        Calendar callCalendar = Utils.convertStringToCalendar(firstAlarmTime, dateFormat);
-        // Creating call alarms
-        for (int i = 0; i < selectedContacts.size(); i++) {
-            Utils.setRecurringAlarm(context, selectedContacts.get(i).getContactNumber(),
-                    selectedContacts.get(i).getContactId(), callCalendar);
-            if (i != selectedContacts.size() - 1)
-                callCalendar.add(Calendar.MINUTE, 1);
+        String firstAlarmTime = Utils.getFirstAlarmTime(context);
+        Log.d("CallingService", "onReceive:instance:" + Calendar.getInstance());
+        if (!firstAlarmTime.isEmpty() &&
+                !Utils.anotherCallScheduled(context, Calendar.getInstance()) &&
+                Utils.isAlarmsScheduled(context)) {
+            Log.d("CallingService", "FirstAlarmTime: " + firstAlarmTime);
+
+            //Personal Alarm
+            Utils.setPersonalAlarm(context, firstAlarmTime);
+            DatabaseHelper databaseHelper = new DatabaseHelper(context);
+            ArrayList<Contact> selectedContacts = databaseHelper.loadContacts();
+            Calendar callCalendar = Utils.convertStringToCalendar(firstAlarmTime, dateFormat);
+            // Creating call alarms
+            for (int i = 0; i < selectedContacts.size(); i++) {
+                Utils.setRecurringAlarm(context, selectedContacts.get(i).getContactNumber(),
+                        selectedContacts.get(i).getContactId(), callCalendar);
+                if (i != selectedContacts.size() - 1)
+                    callCalendar.add(Calendar.MINUTE, 1);
+            }
+            Utils.setLastCallTime(context, dateFormat.format(callCalendar.getTime()));
         }
-        Utils.setLastCallTime(context, dateFormat.format(callCalendar.getTime()));
     }
 }
